08c5b0c8d07021ed7c7ae119457309d59e42a1c4
marcomolisano <fair.geht.vor2013@gmail.com>
2023-02-19 16:01:41 +0100

    Initial Commit

---
+++ LifeMessengerWeConnect/LifeMessengerWeConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeMessengerWeConnectApp.swift
+//  LifeMessengerWeConnect
+//
+//  Created by Marco Molisano on 19.02.23.
+//
+
+import SwiftUI
+
+@main
+struct LifeMessengerWeConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
