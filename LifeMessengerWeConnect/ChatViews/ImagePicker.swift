a446ec1c46ca832caeaf5721219f57e87ec43487
marcomolisano <fair.geht.vor2013@gmail.com>
2023-04-17 13:30:17 +0200

    update 17.04.23

---
+++ LifeMessengerWeConnect/ChatViews/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  LifeMessengerWeConnect
+//
+//  Created by Marco Molisano on 17.04.23.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
--- LifeMessengerWeConnect/Media/PictureAndVideoGalerieView.swift
+++ LifeMessengerWeConnect/Media/PictureAndVideoGalerieView.swift
@@ -4,14 +4,59 @@
 //
 //  Created by Marco Molisano on 02.03.23.
 //
-
+import PhotosUI
 import SwiftUI
 
 struct PictureAndVideoGalerieView: View {
+    @State var selectedItems: [PhotosPickerItem] = []
+    @State var data: Data?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack{
+            Text("Foto Galerie")
+            Image("Messenger")
+                .resizable()
+                .frame(width: 130, height: 140 )
+                .cornerRadius(50)
+            if let data = data, let uiimage = UIImage(data: data) {
+                Image(uiImage: uiimage)
+                    .resizable()
+            }
+        
+            Spacer()
+            PhotosPicker(selection: $selectedItems,
+                         matching: .images) {
+                Text("Foto auswählen")
+                
+            }
+                         .background(Color.white)
+                         .shadow(radius: 5)
+                         .frame(width: 300, height: 30)
+                         .padding(20)
+                         .onChange(of: selectedItems) { newValue in
+                             guard let item = selectedItems.first else {
+                                 return
+                             }
+                             item.loadTransferable(type: Data.self) { result in
+                                 switch result {
+                                 case .success(let data):
+                                     if let data = data {
+                                         self.data = data
+                                         
+                                     } else {
+                                         print("No Data")
+                                     }
+                                 case .failure(let failure):
+                                     fatalError("\(failure)")
+                        
+                                 }
+                             }
+                         }
+                     }
+               }
+            }
+                         
+
 
 struct PictureAndVideoGalerieView_Previews: PreviewProvider {
     static var previews: some View {
